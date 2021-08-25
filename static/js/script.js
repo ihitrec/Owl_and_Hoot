74b17060d849bffb22d3465a160d670eb5da6a93
ihitrec <hitrec555@gmail.com>
2021-08-25 02:06:39 +0000

    fix(nav menu): prevent rapid clicks, adjust animation

--- static/js/script.js
+++ static/js/script.js
@@ -3,16 +3,20 @@ $("svg").click(toggleMenu);
 
 function toggleMenu() {
 
+    $(".menu-btn").css("pointer-events", "none")
     let timeframe;
 
     if ($("nav").height() < 250) {
+        console.log(4)
+
         $("nav").animate({
-            height: "250px"
+            height: "281px"
         }, 500);
         timeframe = 10;
     } else {
+        console.log(5)
         $("nav").animate({
-            height: "120px"
+            height: "155px"
         }, 300);
         timeframe = 300;
     }
@@ -22,5 +26,9 @@ function toggleMenu() {
         $(".login").toggleClass("second");
         $(".cart").toggleClass("third");
     }, timeframe)
+
+    setTimeout(function () {
+        $(".menu-btn").css("pointer-events", "initial")
+    }, 500)
 
 }
