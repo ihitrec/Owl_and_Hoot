c9a81a8f504e175f9f48897fb85b1185b625a2fa
ihitrec <hitrec555@gmail.com>
2021-09-03 23:39:58 +0000

    feat(products app): add model, migrate and create first product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    sale_price = models.DecimalField(
+        blank=True, max_digits=6, decimal_places=2)
+    brand = models.CharField(max_length=20)
+    img = models.URLField()
+    total_rating = models.DecimalField(
+        default=0, max_digits=65, decimal_places=0)
+    num_of_ratings = models.DecimalField(
+        default=0, max_digits=65, decimal_places=0)
+    rating = models.DecimalField(default=0, max_digits=3, decimal_places=2)
+    gender = models.CharField(max_length=6)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
