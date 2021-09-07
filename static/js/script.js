87a0b1eb52c9d00cd090f7a8fb2ec573f42213b1
ihitrec <hitrec555@gmail.com>
2021-09-07 20:55:52 +0000

    feat(rating filter): enable filter functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,71 @@
-// Animate nav dropdown on menu btn click
-$("svg").click(toggleMenu);
+$(document).ready(function () {
 
-function toggleMenu() {
+    // Animate nav dropdown on menu btn click
+    $("svg").click(toggleMenu);
 
-    $(".menu-btn").css("pointer-events", "none")
-    let timeframe;
+    function toggleMenu() {
 
-    if ($("nav").height() < 250) {
-        $("nav").animate({
-            height: "281px"
-        }, 500);
-        timeframe = 10;
-    } else {
-        $("nav").animate({
-            height: "155px"
-        }, 300);
-        timeframe = 300;
+        $(".menu-btn").css("pointer-events", "none")
+        let timeframe;
+
+        if ($("nav").height() < 250) {
+            $("nav").animate({
+                height: "281px"
+            }, 500);
+            timeframe = 10;
+        } else {
+            $("nav").animate({
+                height: "155px"
+            }, 300);
+            timeframe = 300;
+        }
+
+        setTimeout(function () {
+            $(".main-links").toggleClass("first");
+            $("#initial").toggleClass("order");
+            $(".login").toggleClass("second");
+            $(".cart").toggleClass("third");
+        }, timeframe)
+
+        setTimeout(function () {
+            $(".menu-btn").css("pointer-events", "initial")
+        }, 500)
+
     }
 
-    setTimeout(function () {
-        $(".main-links").toggleClass("first");
-        $("#initial").toggleClass("order");
-        $(".login").toggleClass("second");
-        $(".cart").toggleClass("third");
-    }, timeframe)
+    /* Filtering */
 
-    setTimeout(function () {
-        $(".menu-btn").css("pointer-events", "initial")
-    }, 500)
+    // Toggle rating checkbox on label click
+    $($(".rating-filter label")).click(toggleRating);
 
-}
+    function toggleRating(event) {
+        event.preventDefault()
+        let currentInput = $($(this).children()[0]);
+        currentInput.prop('checked', (!currentInput.is(':checked')));
+        $(this).toggleClass("checked-rating");
+        filterRating()
+    }
 
-// $(".product").hasClass()
+    // If filter off show all products, else show selected
+    function filterRating() {
+        let inputs = $(".rating-filter input");
+        checked = [];
+        for (i = 0; i < inputs.length; i++) {
+            if ($(inputs[i]).is(':checked')) {
+                checked.push(parseInt($(inputs[i]).val()));
+            }
+        }
+        let products = $(".product");
+        for (i = 0; i < products.length; i++) {
 
-$($(".rating-filter label")).click(toggleRating);
-
-function toggleRating(event) {
-    event.preventDefault()
-    let currentInput = $($(this).children()[0]);
-    currentInput.prop('checked', (!currentInput.is(':checked')));
-    $(this).toggleClass("checked-rating");
-}
+            let productRating = parseInt($(products[i]).find(".rating").text())
+            if (checked.length === 0) {
+                $(".product").css("display", "block");
+            } else if (!(checked.includes(productRating))) {
+                $(products[i]).css("display", "none");
+            } else {
+                $(products[i]).css("display", "block");
+            }
+        }
+    }
+});
