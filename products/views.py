4fc221c50a18e53784a5543575381eeebc4c3c7c
ihitrec <hitrec555@gmail.com>
2021-09-17 02:35:37 +0000

    feat(search): enable form, add search criteria

--- products/views.py
+++ products/views.py
@@ -1,10 +1,44 @@
+# pylint: disable=no-member
 from django.shortcuts import render
 from .models import Product
+from django.db.models import Q
 
 
 def categories(request, category):
     if category in ['all', 'puma', 'nike', 'adidas']:
-        products = Product.objects.all()
+        if 'search' in request.GET:
+
+            # Add lowercase search words to query list
+            search = request.GET['search']
+            query = [x.lower() for x in search.split()]
+
+            # Filter products by gender if phrase or synonym in query
+            if (not set(['male', 'men', 'boy', 'man']).isdisjoint(query)):
+                products = Product.objects.filter(gender='male')
+            elif (not set(
+                    ['female', 'women', 'girl', 'woman']).isdisjoint(query)):
+                products = Product.objects.filter(gender='female')
+            else:
+                products = Product.objects.all()
+
+            # Extend query with synonyms for matched words
+            synonyms = [
+                ['runner', 'runners', 'footwear', 'sneaker', 'sneakers',
+                 'shoe', 'shoes'],
+                ['shirt', 'tee', 't-shirt', 'tshirt'],
+                ['jeans', 'trousers', 'pants', 'denim']]
+            for values in synonyms:
+                if (not set(values).isdisjoint(query)):
+                    query.extend(values)
+
+            # If product name or category is in filter, add to results
+            filter = Q()
+            for word in query:
+                filter |= Q(name__icontains=word)
+                filter |= Q(category__icontains=word)
+            products = products.filter(filter)
+        else:
+            products = Product.objects.all()
     elif category == 'female':
         products = Product.objects.filter(gender='female')
     elif category == 'male':
