387359c6b09a89dd26aa552f4f295121521931b1
ihitrec <hitrec555@gmail.com>
2021-09-24 17:20:18 +0000

    fix(checkout): return home if checkout url entered when cart empty

--- checkout/views.py
+++ checkout/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 def checkout(request):
 
-    # Redirect to home if last item removed from cart
-    if 'cart-remove' in request.POST and len(request.session['cart']) == 1:
+    # Return to home if cart empty
+    if 'cart-remove' in request.POST and len(request.session['cart']) == 1 or not request.session['cart']:
         return render(request, 'home/index.html')
 
     return render(request, 'checkout/checkout.html')
