12b2f7d0e0dd1b4587f69cfa28803b3a0ed2482c
ihitrec <hitrec555@gmail.com>
2021-09-24 04:52:27 +0000

    feat(cart): enable update/remove form backend functionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -3,8 +3,20 @@ from products.models import Product
 
 
 def cart_items(request):
-    """Get products, individual prices, total cart price and quantity"""
 
+    # Update specific item qty or remove it from cart
+    if request.method == "POST":
+        if 'cart-update' in request.POST:
+            request.session['cart'][request.POST.get('cart-update')][request.POST.get('size')] = int(request.POST.get('quantity'))
+            request.session.modified = True
+        elif 'cart-remove' in request.POST:
+            product = request.session['cart'][request.POST.get('cart-remove')]
+            del product[request.POST.get('size')]
+            if not product:
+                del request.session['cart'][request.POST.get('cart-remove')]
+            request.session.modified = True
+
+    # Get products, individual prices, total cart price and quantity
     cart = request.session.get('cart', {})
 
     cart_items = {}
