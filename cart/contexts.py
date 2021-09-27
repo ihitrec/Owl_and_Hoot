87c04bcf03fbc4c1390a0606a2a479a62b78fc93
ihitrec <hitrec555@gmail.com>
2021-09-27 23:13:18 +0000

    Add total cost val to checkout
    Context variables available in checkout view to update/delete products from cart and get the total cost needed for stipe.
    Global variables enabled for changing updated status.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,28 +1,40 @@
 # pylint: disable=no-member
 from products.models import Product
 
+updated = False
 
-def cart_items(request):
 
-    updated = False
+def product_update(request):
+    """
+    Update product qty or remove it.
 
-    # Update specific item qty or remove it from cart, change updated
+    If function called from checkout view, changes global updated variable to prevent double call
+    """
+
+    cart = request.session['cart']
+    global updated
     if request.method == "POST":
         if request.session.get('cart'):
             if 'cart-update' in request.POST:
-                request.session['cart'][request.POST.get('cart-update')][request.POST.get('size')] = int(request.POST.get('quantity'))
+                cart[request.POST.get('cart-update')][request.POST.get('size')] = int(request.POST.get('quantity'))
                 request.session.modified = True
                 updated = True
             elif 'cart-remove' in request.POST:
-                product = request.session['cart'][request.POST.get('cart-remove')]
+                product = cart[request.POST.get('cart-remove')]
                 del product[request.POST.get('size')]
                 if not product:
-                    del request.session['cart'][request.POST.get('cart-remove')]
+                    del cart[request.POST.get('cart-remove')]
                 request.session.modified = True
                 updated = True
 
 
-    # Get products, individual prices, total cart price and quantity
+def details(request):
+    """
+    Get products, individual prices, total cart price and quantity.
+
+    Set values as details keys so total can be accessed from the checkout view 
+    """
+
     cart = request.session.get('cart', {})
 
     cart_items = {}
@@ -36,12 +48,29 @@ def cart_items(request):
             prices.append(product.price*num)
             quantity += num
     total = sum(prices)
+    details.cart_items = cart_items
+    details.prices = prices
+    details.total = total
+    details.quantity = quantity
+
+
+def cart_items(request):
+    """Add cart items and details to context"""
+
+    global updated
+    if updated == False:
+        product_update(request)
+
+    details(request)
 
     context = {
-        'cart_items': cart_items,
-        'prices': prices,
-        'total': total,
-        'quantity': quantity,
+        'cart_items': details.cart_items,
+        'prices': details.prices,
+        'total': details.total,
+        'quantity': details.quantity,
         'updated': updated
     }
+
+    updated = False
+
     return context
--- checkout/views.py
+++ checkout/views.py
@@ -1,33 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 
 from .models import Order
 from products.models import Product
 from .forms import OrderForm
-from cart.contexts import cart_items
+from cart.contexts import product_update, details
 
 import stripe
 
-def checkout(request):
-    # Return to home if cart empty
-    if 'cart-remove' in request.POST and len(request.session['cart']) == 1 or not request.session['cart']:
-        return render(request, 'home/index.html')
 
- 
+def checkout(request):
+
+    product_update(request)
+    details(request)
+
+    # Return to home if cart empty
+    if not request.session['cart']:
+        return redirect('home')
+
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    total = 10
+    total = details.total
     stripe_total = round(total * 100)
     stripe.api_key = stripe_secret_key
-    intent=stripe.PaymentIntent.create(
+    intent = stripe.PaymentIntent.create(
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY,
     )
 
     if 'postcode' in request.POST:
         cart = request.session['cart']
-        items={}
+        items = {}
         for product_id in list(cart.keys()):
             items[Product.objects.get(id=product_id).name] = cart[product_id]
         order = Order.objects.create(
@@ -44,9 +48,9 @@ def checkout(request):
             )
 
     context = {
-        'form':OrderForm(),
+        'form': OrderForm(),
         'stripe_public_key': stripe_public_key,
-        'client_secret':intent.client_secret,
+        'client_secret': intent.client_secret,
     }    
 
     return render(request, 'checkout/checkout.html', context)
