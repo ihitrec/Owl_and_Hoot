018c1e7a3689d9c24ff8fc1770b8624933bed772
ihitrec <hitrec555@gmail.com>
2021-09-28 02:49:30 +0000

    refractor(cart price filter): remove unnecessary variables

--- cart/templatetags/list_index.py
+++ cart/templatetags/list_index.py
@@ -2,14 +2,9 @@ from django import template
 register = template.Library()
 
 
-list = []
-
-
 @register.filter
-def list_index(indexable):
+def list_index(list):
     """Loop trough list within a nested loop, don't alter it for reuse."""
 
-    global list
-    list = indexable
     list.append(list[0])
     return list.pop(0)
