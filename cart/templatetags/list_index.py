43b395de44c6244874929ac3dff64dc7407001f0
ihitrec <hitrec555@gmail.com>
2021-09-28 01:48:55 +0000

    fix(cart prices): change template filter for correct looping

--- cart/templatetags/list_index.py
+++ cart/templatetags/list_index.py
@@ -2,6 +2,14 @@ from django import template
 register = template.Library()
 
 
+list = []
+
+
 @register.filter
-def list_index(indexable, i):
-    return indexable[i]
+def list_index(indexable):
+    """Loop trough list within a nested loop, don't alter it for reuse."""
+
+    global list
+    list = indexable
+    list.append(list[0])
+    return list.pop(0)
