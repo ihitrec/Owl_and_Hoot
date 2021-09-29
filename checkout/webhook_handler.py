5bae2bf68979101b4bf2f92ec8835d7e1a6daf4d
ihitrec <hitrec555@gmail.com>
2021-09-29 14:56:00 +0000

    feat: add checkout form data chaching in payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -19,11 +19,24 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
 
-   
+        # pid = intent.id
+        # cart = intent.metadata.cart
+        # save_info = intent.metadata.save_info
+
+        # billing_details = intent.charges.data[0].billing_details
+        # shipping_details = intent.shipping
+        # grand_total = round(intent.charges.data[0].amount / 100, 2)
+
+        # # Clean data in the shipping details
+        # for field, value in shipping_details.address.items():
+        #     if value == "":
+        #         shipping_details.address[field] = None
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
