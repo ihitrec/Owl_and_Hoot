6ed2e97ee672fb8e891c1ff599e977921172782f
ihitrec <hitrec555@gmail.com>
2021-10-01 00:21:22 +0000

    feat(profile): save data on form submit, add msg

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserForm
@@ -8,6 +9,12 @@ def profile(request):
     """ Display the user's profile. """
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST' and 'default_city' in request.POST:
+        form = UserForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile successfully updated')
 
     form = UserForm(instance=profile)
     orders = profile.orders.all()
