6e5190da48a1ed86b1580696741e35a56579a2af
ihitrec <hitrec555@gmail.com>
2021-10-01 02:18:54 +0000

    feat(profile): add order history and msg, connect urls

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserForm
+from checkout.models import Order
 
 
 def profile(request):
@@ -24,3 +25,19 @@ def profile(request):
     }
 
     return render(request, 'profiles/profile.html', context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, pk=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, 'checkout/checkout_success.html', context)
