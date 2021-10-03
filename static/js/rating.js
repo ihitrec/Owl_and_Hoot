ad6240d95c485980d27135bfae7ba1cd44296237
ihitrec <hitrec555@gmail.com>
2021-10-03 18:02:44 +0000

    fix(rating.js): change parameters to accurately generate rating imgs

--- static/js/rating.js
+++ static/js/rating.js
@@ -1,33 +1,38 @@
-// Loop trough ratings and add star imgs if rating exists 
-let ratings = $(".rating")
-for (i = 0; i < ratings.length; i++) {
-    let ratingParsed = parseFloat(ratings[i].innerHTML)
-    if (ratingParsed > 0) {
-        generateImages(ratingParsed, i)
-    } else {
-        ratings[i].innerHTML = ''
-    }
-}
-
-// Generate stars, half star if rating over .5
-function generateImages(rating, target) {
-    let ratingNum = ratings[i].innerHTML;
-    ratings[i].innerHTML = '';
-    let roundedRating = Math.round(rating);
-    if (roundedRating <= rating) {
-        for (i = 0; i < roundedRating; i++) {
-            fullStar(target);
+$(document).ready(function () {
+    // Loop trough ratings and add star imgs if rating exists 
+    let ratings = $(".rating")
+    console.log(ratings)
+    for (i = 0; i < ratings.length; i++) {
+        let ratingParsed = parseFloat(ratings[i].innerHTML)
+        console.log(ratingParsed)
+        console.log(i)
+        if (ratingParsed > 0) {
+            generateImages(ratingParsed, i)
+        } else {
+            ratings[i].innerHTML = ''
         }
-    } else if (roundedRating > rating) {
-        let i = 0;
-        for (i; i < roundedRating - 1; i++) {
-            fullStar(target);
-        }
-        $($(".rating")[target]).append("<img src='https://owl-and-hoot.s3.amazonaws.com/static/images/star-half-full.png'></img>");
     }
-    $($(".rating")[target]).append(`<p>${ratingNum}</p>`);
-}
 
-function fullStar(target) {
-    $($(".rating")[target]).append("<img src='https://owl-and-hoot.s3.amazonaws.com/static/images/star-full.png'></img>");
-}
+    // Generate stars, half star if rating over .5
+    function generateImages(rating, target) {
+        let ratingNum = ratings[target].innerHTML;
+        ratings[target].innerHTML = '';
+        let roundedRating = Math.round(rating);
+        if (roundedRating <= rating) {
+            for (x = 0; x < roundedRating; x++) {
+                fullStar(target);
+            }
+        } else if (roundedRating > rating) {
+            let x = 0;
+            for (x; x < roundedRating - 1; x++) {
+                fullStar(target);
+            }
+            $($(".rating")[target]).append("<img src='https://owl-and-hoot.s3.amazonaws.com/static/images/star-half-full.png'></img>");
+        }
+        $($(".rating")[target]).append(`<p>${ratingNum}</p>`);
+    }
+
+    function fullStar(target) {
+        $($(".rating")[target]).append("<img src='https://owl-and-hoot.s3.amazonaws.com/static/images/star-full.png'></img>");
+    }
+});
