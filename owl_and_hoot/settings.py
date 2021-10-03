44d83b205b5f9e8055edacb8825e8a666ef0fa88
ihitrec <hitrec555@gmail.com>
2021-10-03 12:35:57 +0000

    fix: conditionally set debug depending on environment

--- owl_and_hoot/settings.py
+++ owl_and_hoot/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = 'DEVELOPMENT' in os.environ
+print(DEBUG)
 ALLOWED_HOSTS = ['owl-and-hoot.herokuapp.com', 'localhost']
 
 
